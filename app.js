e2996684ac6269d5c94bd548df66c617c21377a6
David Christoffersen <d@vidc.dk>
2021-03-11 13:52:42 -0500

    App js route setup scaffold

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const routes = require('./routes/index');
+
+const PORT = 3000;
 /* require('dotenv').config();
 
 const mongoose = require('mongoose'); // importing mongoose
@@ -7,4 +10,9 @@ const cors = require('cors'); */
 
 const app = express();
 
-app.use();
+app.use('/', routes);
+
+/* #TODO delete the console.log...not allowed */
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
