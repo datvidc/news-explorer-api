a04ae76d1748a6b4e1ed832ecf361c7b4413a933
David Christoffersen <d@vidc.dk>
2021-03-11 13:38:10 -0500

    Setup article schema and model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-/* require('dotenv').config();
 const express = require('express');
+/* require('dotenv').config();
+
 const mongoose = require('mongoose'); // importing mongoose
 const { celebrate, Joi, errors } = require('celebrate');
 const cors = require('cors'); */
