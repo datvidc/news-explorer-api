8ef61725ef16206ba79c78b2d3217710a4108eb5
David Christoffersen <d@vidc.dk>
2021-03-11 13:14:51 -0500

    Enabling validator

--- models/user.js
+++ models/user.js
@@ -1,8 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
 const userSchema = new mongoose.Schema({
+  name: { //  name — username, string from 2 to 30 characters, required field
+    type: String,
+    /* required: true,  all fields are optional by default */
+    minlength: 2,
+    maxlength: 30,
+    default: 'Jacques Cousteau',
+  },
 
+  password: {
+    type: String,
+    required: true,
+    select: false, // from lesson auth part 2
+  },
 
-
-
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function validemail(email) {
+        return validator.isEmail(email);
+      },
+      message: 'Kindly use a valid email address',
+    },
+  },
+});
 
 module.exports = mongoose.model('user', userSchema);
