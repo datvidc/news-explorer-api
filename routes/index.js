3e785be0f1c3b0a95a7d773ac00df3e5d7922abb
David Christoffersen <d@vidc.dk>
2021-03-15 14:06:25 -0400

    Updating signin controller

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const Articlerouter = require('./articles');
 
+router.use('/signin', signinRouter);
+
 router.use('/articles', Articlerouter);
 
 module.exports = router;
