d5acfa41d9ea30929b7d787335b9ebf501851b1c
David Christoffersen <d@vidc.dk>
2021-03-17 21:19:24 -0400

    ES LINTING

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const app = require('./app.js');
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
+  // TODO remove all console logs
   console.log(`App listening on port ${PORT}`);
 });
